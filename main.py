648870a8bd7f882ecd4957f5aabbaf45bd7496ae
cozycornerpatios-fabric <cozycornerpatios@gmail.com>
2025-09-05 00:28:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ load_dotenv()
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
